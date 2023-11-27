b5d58d9061de79a57e65cd446fa044d08cb7519e
cassieie <f21h1234@gmail.com>
2023-11-27 23:27:24 +0800

    review2

--- review.py
+++ review.py
@@ -8,3 +8,12 @@ with open ('reviews.txt', 'r') as f:
 			print(len(data))
 print(len(data))
 print(data[10])
+s = 0
+for d in data:
+	s += len(d)
+print(s/len(data))
+new = []
+for n in data:
+	if len(n) < 100:
+		new.append(n)
+print(len(new))
